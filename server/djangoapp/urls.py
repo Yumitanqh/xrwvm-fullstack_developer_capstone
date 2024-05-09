30d517bd895b089a17d5c9f0eaf257e2e52bc234
yumitanqh <yumitanqh@gmail.com>
2024-05-09 01:23:04 -0400

    changes from week 3 part 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,10 +11,15 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
     path(route='logout',  view=views.logout_request, name="logout"),
 
     # path for dealer reviews view
 
     # path for add a review view
+    
+    # path for get_cars view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
